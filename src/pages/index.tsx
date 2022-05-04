48c0b5afaf5e09fb564018171727923f45d0e60d
Valdielson Siqueira <valdielson.silva@gmail.com>
2022-05-03 23:06:32 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,60 @@
+import { Button, Flex, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+      >
+        <Flex
+          as="form"
+          w="100%"
+          maxWidth={360}
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDir="column"
+        >
+          <Stack spacing="4">
+            <FormControl>
+              <FormLabel htmlFor="email">E-mail</FormLabel>
+              <Input
+                name="email"
+                id="email"
+                type="email"
+                placeholder="Insira seu email" 
+                focusBorderColor="pink.500"
+                bg="gray.900"
+                variant="filled"
+                _hover={{
+                  bgColor: "gray.900"
+                }}
+                size="lg"
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">Senha</FormLabel>
+              <Input
+                name="password"
+                id="password"
+                type="password"
+                placeholder="Insira sua senha"
+                focusBorderColor="pink.500" 
+                bg="gray.900"
+                variant="filled"
+                _hover={{
+                  bgColor: "gray.900"
+                }}
+                size="lg"
+              />
+            </FormControl>
+            <Button type="submit" mt="6" colorScheme="pink" size="lg">
+              Entrar
+            </Button>
+          </Stack>
+        </Flex>
+    </Flex>
+  )
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "900": "#181b23",
+      "800": "#1f2029",
+      "700": "#353646",
+      "600": "#4b4d63",
+      "500": "#616480",
+      "400": "#797d9a",
+      "300": "#9699B0",
+      "200": "#B3B5C6",
+      "100": "#D1D2DC",
+      "50": "#EEEEF2",
+    }
+  },
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto'
+  },
+  styles: {
+    global: {
+      body: {
+        bg:'gray.900',
+        color:'gray.50',
+      }
+    }
+  }
+});

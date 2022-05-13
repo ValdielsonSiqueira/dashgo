e9c1e2f89b77029facbe798d19146b4dd6027f4d
Valdielson Siqueira <valdielson.silva@gmail.com>
2022-05-12 22:28:05 -0300

    refactor(tech): refatoracao do componente header

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,15 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+export function Profile() {
+  return (
+    <Flex align="center">
+      <Box mr="4" textAlign="right">
+        <Text>Valdi Siqueira</Text>
+        <Text color="gray.300" fontSize="small">
+        valdielson.silva@gmail.com
+        </Text>
+      </Box>
+      <Avatar size="md" name="Valdi Siqueira" src="https://avatars.githubusercontent.com/u/35588487?v=4" />
+    </Flex>
+  );
+}

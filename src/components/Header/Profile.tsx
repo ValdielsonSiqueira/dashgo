65cd664496a5ce3678b4c7ef8fe2ed8af25e5d4a
Valdielson Siqueira <valdielson.silva@gmail.com>
2022-05-16 20:24:26 -0300

    feat(product): responsividade do componente header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Valdi Siqueira</Text>
-        <Text color="gray.300" fontSize="small">
-        valdielson.silva@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Valdi Siqueira</Text>
+          <Text color="gray.300" fontSize="small">
+          valdielson.silva@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Valdi Siqueira" src="https://avatars.githubusercontent.com/u/35588487?v=4" />
     </Flex>
   );

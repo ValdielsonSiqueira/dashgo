50d15e8298d48bba5c8508fec014717dbcdaad44
Valdielson Siqueira <valdielson.silva@gmail.com>
2022-06-26 18:09:10 -0300

    feat(product): controle de form na pagina de signin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,19 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
 import { Input } from '../components/Form/Input';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> =async(values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(values);
+  }
   return (
     <Flex
       w="100vw"
@@ -16,11 +29,12 @@ export default function SignIn() {
           p="8"
           borderRadius={8}
           flexDir="column"
+          onSubmit={handleSubmit(handleSignIn)}
         >
           <Stack spacing="4">
-            <Input type="email" name="email" label="Email" placeholder="Insira seu email"  />
-            <Input type="password" name="password" label="Senha" placeholder="Insira sua senha"  />
-            <Button type="submit" mt="6" colorScheme="pink" size="lg">
+            <Input type="email" name="email" label="Email" placeholder="Insira seu email"  {...register('email')} />
+            <Input type="password" name="password" label="Senha" placeholder="Insira sua senha" {...register('senha')}  />
+            <Button type="submit" mt="6" colorScheme="pink" size="lg" isLoading={formState.isSubmitting}>
               Entrar
             </Button>
           </Stack>

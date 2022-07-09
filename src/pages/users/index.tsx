6d80df7765ea1430f170da8ebf96d4d6f7286cb3
Valdielson Siqueira <valdielson.silva@gmail.com>
2022-07-09 20:52:28 -0300

    feat(product): adicionado ao cache a listagem de usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,20 @@
-import { Box, Icon, Button,Text, Flex, Heading, Table, Thead, Th, Checkbox, Tr, Tbody, Td, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Icon, Button,Text, Flex, Heading, Table, Thead, Th, Checkbox, Tr, Tbody, Td, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect } from "react";
 import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useQuery } from 'react-query';
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response =  await fetch('http://localhost:3000/api/users')
+    const data = await response.json();
+
+    return data;
+  })
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -40,32 +48,44 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" w="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th> }
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Valdi Siqueira</Text>
-                    <Text fontSize="sm" color="gray.300">valdielson.silva@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>12 de maio de 2022</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao carregar dados dos usuarios</Text>
+            </Flex>
+          ): (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th> }
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Valdi Siqueira</Text>
+                        <Text fontSize="sm" color="gray.300">valdielson.silva@gmail.com</Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>12 de maio de 2022</Td>}
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>  
     </Box>

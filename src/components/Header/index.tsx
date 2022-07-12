0d2095acd2a117c071967e8ab44a726f7e3ab0e5
Valdielson Siqueira <valdielson.silva@gmail.com>
2022-07-11 21:25:12 -0300

    feact(product) atualizando listagem de usuarios

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -33,9 +33,7 @@ export function Header() {
           variant="unstyled"
           onClick={onOpen}
           mr="2"
-        >
-
-        </IconButton>
+        />
       )}
       <Logo />
       { isWideVersion && <SearchBox />}
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,14 +1,14 @@
-import { Box, Icon, Button,Text, Flex, Heading, Table, Thead, Th, Checkbox, Tr, Tbody, Td, useBreakpointValue, Spinner } from "@chakra-ui/react";
+import { Box, Stack, Icon, Button,Text, Flex, Heading, Table, Thead, Th, Checkbox, Tr, Tbody, Td, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect } from "react";
-import { RiAddLine } from "react-icons/ri";
+import { RiAddLine, RiFileDownloadLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { useQuery } from 'react-query';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
+  const { data, isLoading, isFetching, error, refetch } = useQuery('users', async () => {
     const response =  await fetch('http://localhost:3000/api/users')
     const data = await response.json();
     const users = data.users.map(user => ({ ...user, createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', { 
@@ -41,18 +41,33 @@ export default function UserList() {
         <Sidebar />
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Lista de Usuários</Heading>
-            <Link href="/users/create" passHref>
+            <Heading size="lg" fontWeight="normal">
+              Lista de Usuários
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+            </Heading>
+            <Stack direction="row" spacing="6">
               <Button 
-                as="a" 
-                size="sm" 
-                fontSize="sm" 
-                colorScheme="pink"
-                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-              >
-                Criar novo
+                  as="a" 
+                  size="sm" 
+                  fontSize="sm" 
+                  colorScheme="pink"
+                  leftIcon={<Icon as={RiFileDownloadLine} fontSize="20" />}
+                  onClick={refetch}
+                >
+                  Atualizar Lista
               </Button>
-            </Link>
+              <Link href="/users/create" passHref>
+                <Button 
+                  as="a" 
+                  size="sm" 
+                  fontSize="sm" 
+                  colorScheme="pink"
+                  leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                >
+                  Criar novo
+                </Button>
+              </Link>
+            </Stack>
           </Flex>
           { isLoading ? (
             <Flex justify="center">

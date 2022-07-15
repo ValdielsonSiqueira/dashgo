eddb97598aeb8481c1a7740f454397112ac4c1b7
Valdielson Siqueira <valdielson.silva@gmail.com>
2022-07-14 21:54:36 -0300

    feat(product): adicionando axios para realizar chamadas

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,11 +6,11 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { useQuery } from 'react-query';
+import { api } from "../../services/api";
 
 export default function UserList() {
   const { data, isLoading, isFetching, error, refetch } = useQuery('users', async () => {
-    const response =  await fetch('http://localhost:3000/api/users')
-    const data = await response.json();
+    const { data } =  await api.get('users')
     const users = data.users.map(user => ({ ...user, createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', { 
       day: '2-digit',
       month: 'long',
@@ -18,7 +18,7 @@ export default function UserList() {
     })}))
     return users;
   }, {
-    staleTime: 1000 * 5,
+   
   }) 
 
   const isWideVersion = useBreakpointValue({
